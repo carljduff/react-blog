1eda606b4415028a30f9d36b5b28886ce828ad6e
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-04-09 18:12:40 +0000

    created intro

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,30 @@
 const About = () => {
     return(
-        <div>
-            <p className="about" id='about'>Hello, my name is Jordan Duff. I'm currently a student training to become a Full-Stack Web Developer. 
-            I have lived in Lexington, KY for the past four years.
+        <div className="about">
+            <div className="about-left"> 
+            
+                <div className="about-card bg"></div>
+                    
+                    <div className="about-card">
+                    <img src='https://images.unsplash.com/photo-1565879629766-30adf38aac56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80' className="about-img"/>
+                    </div>
+            
+            </div>
+
+
+            <div className="about-right">
+                <h1 className="a-title">About Me</h1>
+                <p className="about-sub">I'm currently a student training to become a Full-Stack Web Developer.</p>
+                <p className="about-desc"> I have lived in Lexington, KY for the past four years.
             I have an extensive background in Restaurant Management and my ultimate goal is to work on or help create the next Restaurant system.
             I am passionate about understanding the ins and outs of Developing and consider myself a 'forever student'! I love taking the things 
             I learn about and seeing how they work in my life and the choices I make in my day to day. I am looking forward to my future and cannot
             wait to travel on the path ahead!</p>
+                
+            </div>
         </div>
     )
 }
 
 export default About;
+            
---
+++ src/pages/Intro.js
@@ -0,0 +1,40 @@
+import Me from "../components/jordand.png"
+
+
+const Intro = () => {
+    return(
+        <div className="i">
+        <div className="i-left">
+            <div className="i-left-wrapper">
+                <h1 className="i-intro">Hi, my name is</h1>
+                <h2 className="i-name">Jordan Duff</h2>
+                <div className="i-title">
+                    <div className="i-title-wrapper">
+                        <div className="i-title-item">Front End Web Developer</div>
+                        <div className="i-title-item">Back End Web Developer</div>
+                        <div className="i-title-item">Life-time Learner</div>
+                        <div className="i-title-item">Piano Player</div>
+                    </div>
+                </div>
+                <p className="i-desc">
+                    This is some text that will later be filled in. This is some text that will later be filled in.
+                    This is some text that will later be filled in. This is some text that will later be filled in.
+                    This is some text that will later be filled in. This is some text that will later be filled in.
+                    This is some text that will later be filled in. This is some text that will later be filled in.
+                    This is some text that will later be filled in. This is some text that will later be filled in.
+
+                </p>
+
+            </div>
+            </div>
+        <div className="i-right">
+            <div className="i-bg"></div>
+            <img src={Me} className="i-img" />
+
+        </div>
+        </div>
+
+    )
+}
+
+export default Intro
