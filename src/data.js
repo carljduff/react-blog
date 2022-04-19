5cc379410ee3345031122757d54ada687b9e8c50
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-04-19 00:14:09 +0000

    created blog data

--- src/data.js
+++ src/data.js
@@ -33,3 +33,113 @@ export const projects = [
     link: "https://github.com/carljduff/sql-heroes",
 },
 ]
+
+export const blogs = [
+    {
+        id: 1,
+        week: 'Week One', 
+        title: 'Git & GitHub',
+        date: 'February 28, 2022',
+        description: "I am so excited to finally get started. Before we started the bootcamp, we got a breakdown of what we'd be doing weekly. I started researching these topics and I jumped into Git and Github a week before it started. I remember thinking 'there's no way I'll ever get this'. Commit, merge, push, pull, clone, init... the list goes on. I watched a tutorial on YouTube, but still didn't get the point of it. After this week, I fully get the point of it, why it matters, I see the ease of access and keeping everything organized. Also, those once haunting words, now make sense and I know when to use them, and the purpose of their use. This was a great week and I'm motivated for next!",
+        post: ""
+    },
+
+    {
+        id: 2,
+        week: 'Week Two', 
+        title: 'HTML & CSS',
+        date: 'February 04, 2022',
+        description: "Understanding how HTML and CSS works together has been interesting. My favorite thing about this week was using CSS and Flexbox. I love styling pages and it really took me back to my Myspace days! I knew basic HTML & CSS before this week, so I dove into deeper subjects. This week our project was to recreate a Birdwatching website. I choose to do different colors and styling because I didn't enjoy the colors of the original, but I'm sure someone out there does! It was rewarding to finally have a project to put together and for it to actually come together. I plan on finding some cool things to do with CSS. I'm nervous for next week because there's an alarm clock project and the first thing we've done without any hand holding! I can do this....I think.",
+        post: ""
+    },
+
+    {
+        id: 3,
+        week: 'Week Three', 
+        title: 'JavaScript',
+        date: 'February 11, 2022',
+        description: "This week was Javascript and at the very least, I'm happy to say that I got through it. It was challenging, there were moments that I wanted to scream (or did scream), and there were also moments of success! When the Javascript code that I wrote worked, I wanted to run around my house! It was such a great feeling. We were pushed to the limits this week. There were two projects this week. A silly story generator and an alarm clock. The silly story generator was essentially a walkthrough project, but the alarm clock was from scratch. When I first started, I stared at a blank screen for at least thirty minutes wrapping my head around how I was even going to start this, let alone finish. I started breaking it down. I thought about the functionality of an actual alarm clock. What does it need? ...and that's where I started. I researched, took many notes, bits and pieces of information from different resources and put it all together. I'm not 100% satisfied with what I presented, but it worked and it was good enough for me now. I plan on revisitng this project as I progress. I know it's only going to get more challenging from here.",
+        post: ""
+    },
+
+    {
+        id: 4,
+        week: 'Week Four', 
+        title: '',
+        date: '',
+        description: "",
+        post: ""
+    },
+
+    {
+        id: 5,
+        week: 'Week Five', 
+        title: 'Front End/Vanilla JavaScript',
+        date: 'February 25, 2022',
+        description: "The first week of the In-Person portion of the Bootcamp just came to an end! I learned and researched more in this week than I ever have in my entire life. I am so thankful for this opportunity and this has been a great week! I was nervous at first, but after getting into it I realized how comforting everyone is. They truly want us to succeed. I am excited for the next couple of weeks to come, but I know there are many challenges ahead.",
+        post: ""
+    },
+
+    {
+        id: 6,
+        week: 'Week Six', 
+        title: "Front End/API's",
+        date: 'March 4, 2022',
+        description: "This week was all about API's and asynchronous functions. We went over Fetch, Promises, JSON, Axios and I'm sure I'm leaving something else out. It was a lot to take in and I added some topics to my back-log to gain a deeper understanding. The project for this week was building a Weather API from scratch. I was impressed by how much I truly learned last week that led me into this week. ",
+        post: ""
+    },
+
+    {
+        id: 7,
+        week: 'Week Seven', 
+        title: '',
+        date: 'March 11, 2022',
+        description: "",
+        post: ""
+    },
+
+    {
+        id: 8,
+        week: 'Week Eight', 
+        title: 'Front End/React Basics',
+        date: 'March 18, 2022',
+        description: "This was the first week we broke away from Vanilla Javascript. At the beginning of the week, the sense of dread was captivating about leaving the known for the unknown. After Tuesday, diving into the docs for React and starting a walk-thru tutorial, I had a better understanding and had a basic grasp. This was a week that I will remember because it has stood out to me the most. The difference in how I felt at the beginning of the week compared to the end was astronomical. I saw concepts coming together for one of the first times since in the Bootcamp. This weeks project was to build a Restaurant Menu and pull the menu data from an API. It was the first week we had a lot of control on how we styled our application. This was one of my favorite weeks so far. So excited for next week to dive further into React!",
+        post: ""
+    },
+
+    {
+        id: 9,
+        week: 'Week Nine', 
+        title: 'Front End/Frameworks',
+        date: 'March 25, 2022',
+        description: "This week was all about React and a to-do list. It was a tough week, but I was happy with the outcome. I learned a lot of valuable things this week that I will carry on into next week!",
+        post: ""
+    },
+
+    {
+        id: 10,
+        week: 'Week Ten', 
+        title: 'Python/Pair Programming',
+        date: 'April 1, 2022',
+        description: "",
+        post: ""
+    },
+
+    {
+        id: 11,
+        week: 'Week Eleven', 
+        title: 'Backend/Frameworks',
+        date: 'April 8, 2022',
+        description: "",
+        post: ""
+    },
+
+    {
+        id: 12,
+        week: 'Week Twelve', 
+        title: '',
+        date: '',
+        description: "",
+        post: ""
+    }
+]
